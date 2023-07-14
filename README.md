8c781f4e8d1ed78f7f2ac6c1e5168d4810efb56b
Min Ji Seo <seominji92@gmail.com>
2023-07-14 05:46:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Snakemon Go
+
+This program is a Pokemon-themed snake game implemented using HTML, CSS, and JavaScript. It allows players to control a pokeball on a game board, catch Pokemon, and earn points.
+
+## Getting Started
+
+To run the game, follow these steps:
+
+1. Ensure you have a compatible web browser installed on your machine.
+2. Download the program files, including HTML, CSS, and JavaScript.
+3. Open the HTML file (`index.html`) in your web browser.
+
+## Gameplay Instructions
+
+- Control the snake's movement using the arrow keys or by clicking the directional buttons on the screen (in media view < 800px).
+- Your goal is to guide the pokeball to catch the pokemon randomly displayed on the game board.
+- Each time the pokeball catches a pokemon, your pokeball collection will grow longer, and you will earn points.
+- The game ends if the pokeball collides with the game board boundaries or its own chain of pokeballs.
+- The highest score achieved during gameplay will be recorded as the high score.
+
+## Dependencies
+
+This program relies on the following external resources:
+
+- Google Fonts: The game uses a specific font from Google Fonts. Make sure you have an active internet connection to load the font correctly.
+
+- Google Material Icons: The game utilizes icons from the Google Material Icons library. Ensure you have an active internet connection to load the icons properly.
+
+## Compatibility
+
+The game is designed to be compatible with most modern web browsers. Ensure that your web browser is up to date for optimal performance.
+
+## Acknowledgments
+
+This game was developed as a fun project inspired by the classic snake game and the Pokemon franchise. Special thanks to the creators of Pokemon for the artwork and the developers of the snake game concept.
+
+All pokemon sprites were downloaded from the [Pokemon Database](https://pokemondb.net/)
---
+++ js/script.js
@@ -0,0 +1,116 @@
+const playBoard = document.querySelector(".play-board");
+const scoreBoard = document.querySelector(".score");
+const highScoreBoard = document.querySelector(".high-score");
+const controller = document.querySelectorAll(".controller i")
+
+
+let gameOver = false;
+let pokeX, pokeY;
+let snakeX = 5,
+  snakeY = 5;
+let velocityX = 0,
+  velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+let randomPokemon = "";
+
+// Get high score from local storage or assign 0 and updat display
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreBoard.innerText = `High Score: ${highScore}`;
+
+function randPosition() {
+  // Selecting a random Pokemon from the list
+  let randomIndex = Math.floor(Math.random() * pokemon.length);
+  // Updating the background image URL
+  randomPokemon = pokemon[randomIndex];
+
+  // Passing a random number between 1 - 30 inclusive as food position
+  pokeX = Math.floor(Math.random() * 25) + 1;
+  pokeY = Math.floor(Math.random() * 25) + 1;
+}
+
+function gameOverReset() {
+  // Clear timer and reload page
+  clearInterval(setIntervalId);
+}
+
+function changeDirection(e) {
+  // Change velocity of player movement based on key press
+  if (e.key === "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
+
+controller.forEach(key => {
+  key.addEventListener("click", () => changeDirection({key: key.dataset.key }));
+});
+
+function startGame() {
+  if(gameOver) {
+    // Show the modal
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+  
+    // Set the score in the modal
+    let scoreSpan = document.getElementById('score');
+    scoreSpan.textContent = score;
+    return gameOverReset();
+  } 
+  // Create a food div including update to back-ground image
+  let htmlMarkup = `<div class="food" style="grid-area: ${pokeY} / ${pokeX}; background-image: url('/images/pokemon/${randomPokemon}.gif')"></div>`;
+  // Checking if snake hit the food
+  if (snakeX === pokeX && snakeY === pokeY) {
+    randPosition();
+    // Push food position to snake body array
+    snakeBody.push([pokeX, pokeY]);
+    score++;
+    highScore = Math.max(highScore, score);
+    localStorage.setItem("high-score",highScore);
+    scoreBoard.innerText = `Score: ${score}`;
+    highScoreBoard.innerText = `High Score: ${highScore}`;
+  }
+
+  // Update snake head position based on current velocity
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  // Shifting forward the values of the elements in the snake body by one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+
+  // Set up first element of snake to current snake position
+  snakeBody[0] = [snakeX, snakeY];
+  
+  // Setting gameboard boundary
+  if(snakeX <= 0 || snakeX > 25 || snakeY <=0 || snakeY > 25) {
+    return gameOver = true;
+  }
+
+  // Add div for each part of snake body
+  for (let i = 0; i < snakeBody.length; i++) {
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+    // Game over if snake hits it's own body
+    if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+      gameOver = true;
+    }
+  }
+  // Insert food div into the play-board element
+  playBoard.innerHTML = htmlMarkup;
+
+}
+
+randPosition();
+setIntervalId = setInterval(startGame, 100);
+document.addEventListener("keydown", changeDirection);
